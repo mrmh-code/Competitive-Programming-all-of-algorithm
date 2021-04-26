84aa54efa8478af7a5bb568246b2922dd1e6f688
M-Mehedi-Hasan <hasanalmehedi07@gmail.com>
2021-04-26 09:11:34 +0600

    Bitwise Operation

---
+++ 1.Beginner/3.Bitwise Operation/1.And,Or,Xor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Bitwise And
+   /* a=4=> 0000 0100
+      b=7=> 0000 0111
+      ________________
+      a&b=> 0000 0100(=4)   */
+      int a=4,b=7;
+      cout<<"Bitwise And: a&b="<<(a&b)<<endl;
+
+    /*Bitwise OR
+     a=4=> 0000 0100
+      b=7=>0000 0111
+      ________________
+      a|b=>0000 0111(=7)   */
+
+      cout<<"Bitwise OR: a|b="<<(a|b)<<endl;
+
+    /*Bitwise Xor
+      a=4=>0000 0100
+      b=7=>0000 0111
+      ________________
+      a^b=>0000 0011(=3)*/
+      cout<<"Bitwise XOR: a^b="<<(a^b)<<endl;
+
+
+}
