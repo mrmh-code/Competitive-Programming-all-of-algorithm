7dd69253a0461a6704a27c50dad9718e99fbdd32
M-Mehedi-Hasan <hasanalmehedi07@gmail.com>
2021-05-20 04:50:45 +0600

    inputlikedlist.cpp

---
+++ 1.Beginner/2.Data structure/4.Linked List/2.inputlikedlist.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+///type -1 than exit program 
+class Node{
+    public:
+      int data;
+      Node *next;
+
+      Node(int data){
+        this->data=data;
+        next=NULL;
+      }
+};
+
+Node* takeInput(){
+  int data;
+  cin>>data;
+  Node *head=NULL;
+  Node *tail=NULL;
+
+  while (data!=-1)
+  {
+    Node *n=new Node(data);
+    if (head==NULL)
+    {
+        head=n;
+        tail=n;
+    }else{
+      tail->next=n;
+      tail=n;
+    }
+    cin>>data;
+  }
+    return head;
+}
+void print(Node *&p){
+  while (p!=NULL)
+  {
+    cout<<p->data<<" ";
+    p=p->next;
+  }
+  cout<<endl;
+  
+} 
+int main()
+{
+  Node *head=takeInput();
+  print(head);
+}
